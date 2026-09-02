92fca813d7e682fee21b92c587168ac19deb8a5d
aguscepeda <agustomascepe20@gmail.com>
2026-09-01 22:32:21 -0300

    Complete ejercicios 8,9,10 de la parte 2

--- src/parte 2/clase-alumno.ts
+++ src/parte 2/clase-alumno.ts
@@ -12,8 +12,8 @@
 // -----------------------------------------------------------------------------
 // EJERCICIO 10 - interface Materia
 // -----------------------------------------------------------------------------
-//TODO cambiar a type
-export interface Materia {
+
+export type Materia = {
     codigo: number;
     nombre: string;
     horas: number;
@@ -38,8 +38,11 @@ export class Alumno {
         edad: number,
         email: string
     ) {
-        // TODO (Ejercicio 8): asignar los atributos recibidos.
-        throw new Error("Implementar");
+        this.legajo = legajo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.email = email;
     }
 
     // -------------------------------------------------------------------
@@ -47,13 +50,11 @@ export class Alumno {
     // -------------------------------------------------------------------
 
     getNombreCompleto(): string {
-        // TODO
-        throw new Error("Implementar");
+        return this.nombre + " " + this.apellido;
     }
 
     esMayorDeEdad(): boolean {
-        // TODO
-        throw new Error("Implementar");
+        return this.edad >= 18;
     }
 
     // -------------------------------------------------------------------
@@ -61,15 +62,15 @@ export class Alumno {
     // -------------------------------------------------------------------
 
     getEdad(): number {
-        // TODO
-        throw new Error("Implementar");
+        return this.edad;
     }
 
     setEdad(edad: number): void {
-        // TODO: debe impedir edades inválidas.
-        // edad < 0   -> throw new Error(...)
-        // edad > 120 -> throw new Error(...)
-        throw new Error("Implementar");
+        if (edad < 0 || edad > 120)
+        {
+        throw new Error("Edad inválida");
+        }
+        this.edad = edad;
     }
 
     // -------------------------------------------------------------------
@@ -77,29 +78,44 @@ export class Alumno {
     // -------------------------------------------------------------------
 
     agregarMateria(materia: Materia): void {
-        // TODO
-        throw new Error("Implementar");
+        if (materia == null)
+            throw new Error("Materia nula")
+            
+        this.materias.push(materia);
+        
     }
 
     quitarMateria(codigo: number): Materia | undefined {
-        // TODO: quitar la materia con ese código y devolverla.
-        // Si no está inscripto en ninguna con ese código, devolver undefined.
-        throw new Error("Implementar");
+
+        const materiaEliminar = this.materias.find(materia => materia.codigo == codigo);
+
+        if (materiaEliminar == undefined) {
+            return undefined;
+        }
+
+        const indice = this.materias.indexOf(materiaEliminar);
+        this.materias.splice(indice, 1);
+
+        return materiaEliminar;
     }
 
     estaInscripto(codigo: number): boolean {
-        // TODO
-        throw new Error("Implementar");
+        return this.materias.some(materia => materia.codigo == codigo);
     }
 
     cantidadMaterias(): number {
-        // TODO
-        throw new Error("Implementar");
+        return this.materias.length;
     }
 
     getMaterias(): Materia[] {
         // TODO: devolver las materias sin exponer el arreglo interno
         // (devolver una copia, no la referencia original).
-        throw new Error("Implementar");
+        const materiasCopia: Materia[] = [];
+
+        for (const materia of this.materias) {
+            materiasCopia.push(materia);
+        }
+
+        return materiasCopia;
     }
 }
